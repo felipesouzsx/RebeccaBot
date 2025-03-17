0f51349244304f70be26cf59ddcc7e0e022d2e52
Felipe Souza <im.felipemontsouza@gmail.com>
2025-03-17 13:33:12 -0300

    implement getactivitydata command

---
+++ bot/src/interface/commands/getactivitydata.js
@@ -0,0 +1,35 @@
+const DataBaseAccess = require('../../database/dbAccess.js');
+
+module.exports.staffCommand = true;
+module.exports.description = 'Lista algumas informações';
+
+
+function sendChatMessage(interaction, message) {
+  interaction.guild.channels.cache.get(interaction.channelId).send(message);
+}
+
+
+module.exports.run = async (CLIENT, interaction) => {
+  interaction.reply('BOT_RSP: Gathering data from database...');
+
+  let guildMembers = await DataBaseAccess.getGuildMembers(interaction.guild);
+  let memberIds = Object.keys(guildMembers);
+  let count = 0;
+  let message = '';
+
+  sendChatMessage(interaction, `BOT_RSP: Retrieved a total of ${memberIds.length} members.`);
+
+  for(memberId in memberIds) {
+    let member = guildMembers[memberIds[memberId]];
+    console.log(memberId);
+    if (count == 20) {
+      sendChatMessage(interaction, message);
+      message = '';
+      count = 0;
+    };
+    let lastMessageDate = new Date(member.lastMessageTimestamp);
+    let time = `${lastMessageDate.toLocaleDateString('en-US')}`;
+    message += `${memberId}. ${member.username} = ${time}\n`;
+    count++;
+  }
+}
--- bot/src/interface/commands/getdata.js
+++
@@ -1,6 +0,0 @@
-module.exports.staffCommand = true;
-module.exports.description = 'Lista algumas informações';
-
-module.exports.run = async (CLIENT, interaction) => {
-  interaction.reply('getdata não implementado ainda ;)');
-}
