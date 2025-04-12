a570ff1f3aa08329f8b52673e6279eb98576050d
Felipe Souza <im.felipemontsouza@gmail.com>
2025-04-12 14:39:30 -0300

    [bot] remove log channel option from setup command as it is not necessary

--- bot/src/interface/commands/setup.js
+++ bot/src/interface/commands/setup.js
@@ -3,18 +3,10 @@ const guildDB = require('../../database/guildDb.js');
 
 module.exports.description = 'use isso pra eu me configurar!';
 module.exports.staffCommand = true;
-module.exports.options = {
-  logchannel: {
-    type: 'ChannelOption',
-    description: 'canal de logs!',
-    required: true
-  }
-}
 
 
 module.exports.run = async (CLIENT, interaction) => {
   const GUILD = interaction.guild;
-  //let logChannel = interaction.options['logchannel'].value;
   await guildDB.add(GUILD);
   interaction.reply('Added this guild to my database');
 }
