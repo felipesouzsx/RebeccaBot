255eeb5471678ba39f27819e21353d2f7346303c
Felipe Souza <im.felipemontsouza@gmail.com>
2025-04-12 14:41:19 -0300

    [bot] detect when user joins voice chat

---
+++ bot/src/interface/events/voiceStateUpdate.js
@@ -0,0 +1,15 @@
+const { getCurrentTimestamp } = require('../../util/timeUtil.js');
+const { User } = require('../../util/discordUtil.js');
+const userDB = require('../../database/userDb.js');
+
+
+module.exports = async (CLIENT, ...args) => {
+  const NEW_STATE = args[1];
+  const USER = NEW_STATE.guild.members.cache.get(NEW_STATE.id).user;
+
+  if (USER.bot) { return }
+
+  const nowTimestamp = getCurrentTimestamp();
+  const userData = new User(USER.username, nowTimestamp);
+  userDB.edit(NEW_STATE.guild.id, NEW_STATE.id, userData);
+}
