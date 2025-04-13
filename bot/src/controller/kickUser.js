22d7199c1ad996669a0dbd946939530b975e8f92
Felipe Souza <im.felipemontsouza@gmail.com>
2025-04-13 11:13:23 -0300

    [bot] disable member kicking feature

--- bot/src/controller/kickUser.js
+++ bot/src/controller/kickUser.js
@@ -1,6 +1,6 @@
 module.exports = async (CLIENT, guildId, userId) => {
   let guild = await CLIENT.guilds.cache.get(guildId);
   let member = await guild.members.cache.get(userId);
-  member.kick();
+  // member.kick();
   console.log(`BOT_KCK: Bot kicking user ${member.user.username}`)
 }
