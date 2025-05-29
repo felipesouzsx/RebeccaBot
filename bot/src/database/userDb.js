7acb63f475e8348b945e2d5d1782dedb3e4afe9f
Felipe Souza <im.felipemontsouza@gmail.com>
2025-05-29 11:08:30 -0300

    [bot] revert fetchDatabase call on userDb.get to old format

--- bot/src/database/userDb.js
+++ bot/src/database/userDb.js
@@ -26,19 +26,14 @@ async function remove(guildId, userId) {
 async function edit(guildId, User) {
   let userJson = User.getJson();
   let jsonData = JSON.stringify(userJson);
-  console.log(userJson)
   let response = await dbAccess.fetchDatabase(`/guilds/${guildId}/users/${User.id}`, 'PUT', jsonData);
+  dbAccess.deleteFromCache(`/guilds/${guildId}`); // Removing guild from cache as well
   if (response.status != 200) { throw getStatusError(response.status) }
 }
 
 
 async function get(guildId, userId) {
-  let response = await dbAccess.fetchDatabase(
-    `/guilds/${guildId}/users/${userId}`, 
-    'GET',
-    undefined,
-    false
-  );
+  let response = await dbAccess.fetchDatabase(`/guilds/${guildId}/users/${userId}`);
   if (response.status != 200) { throw getStatusError(response.status) }
   response.data["id"] = userId
   let result = new User(response.data);
